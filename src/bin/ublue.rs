783c53ebb8118e4129eda6a395b047a25e0a1d1e
Gerald Pinder <gmpinder@gmail.com>
2023-09-25 22:45:42 -0400

    Create templates, serialization structs, and cli arg parsing

--- src/bin/ublue.rs
+++ src/bin/ublue.rs
@@ -1,5 +1,8 @@
+use std::{fs, io, path::PathBuf};
+
 use anyhow::Result;
 use clap::{Parser, Subcommand};
+use ublue_rs::Recipe;
 
 #[derive(Parser, Debug)]
 #[command(name = "Ublue Builder", author, version, about, long_about = None)]
@@ -10,11 +13,39 @@ struct UblueArgs {
 
 #[derive(Debug, Subcommand)]
 enum CommandArgs {
-    Template,
-    Build,
+    /// Generate a Containerfile from a recipe
+    Template {
+        /// The recipe file to create a template from
+        #[arg()]
+        recipe: PathBuf,
+
+        /// Optional Containerfile to use as a template
+        #[arg(short, long)]
+        containerfile: Option<PathBuf>,
+    },
+
+    /// Build an image from a Containerfile
+    Build {
+        #[arg()]
+        containerfile: PathBuf,
+    },
 }
 
 fn main() -> Result<()> {
-    UblueArgs::parse();
+    let args = UblueArgs::parse();
+
+    match args.command {
+        CommandArgs::Template {
+            recipe,
+            containerfile,
+        } => {
+            let recipe: Recipe = serde_yaml::from_str(fs::read_to_string(recipe)?.as_str())?;
+            println!("{:#?}", recipe);
+        }
+        CommandArgs::Build { containerfile } => {
+            println!("Not yet implemented!");
+            todo!();
+        }
+    }
     Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use serde::{Deserialize, Serialize};
+
+pub const DEFAULT_CONTAINERFILE: &'static str =
+    include_str!("../templates/starting_point.template");
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Recipe {
+    pub name: String,
+
+    #[serde(rename = "base-image")]
+    pub base_image: String,
+
+    #[serde(rename = "fedora-version")]
+    pub fedora_version: u16,
+
+    pub scripts: Scripts,
+
+    pub rpm: Rpm,
+
+    #[serde(rename = "usr-dir-overlays")]
+    pub usr_dir_overlays: Vec<String>,
+
+    pub containerfiles: Containerfiles,
+
+    pub firstboot: FirstBoot,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Scripts {
+    pub pre: Vec<String>,
+    pub post: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Rpm {
+    pub repos: Vec<String>,
+    pub install: Vec<String>,
+    pub remove: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FirstBoot {
+    pub yafti: bool,
+    pub flatpaks: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Containerfiles {
+    pub pre: Vec<String>,
+    pub post: Vec<String>,
+}
