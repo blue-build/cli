42d879a6e52cc466a1e12d754eaa26ce7b21015a
xyny <60004820+xynydev@users.noreply.github.com>
2024-01-24 22:27:51 +0000

    chore: use GHCR for install.sh (#11)
    Trying to make the action install the binary from the container on GHCR
    with `podman run --rm ghcr.io/blue-build/cli:main-installer | sudo bash`
    (like in the README). Getting some error and this _might_ be related and
    might not, but shouldn't hurt to merge either, since I just made the
    `cli` package public.
    
    ---------
    
    Co-authored-by: Gerald Pinder <gmpinder@gmail.com>

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ function cleanup() {
   echo "Cleaning up image"
   podman stop -i -t 0 blue-build-installer
   sleep 2
-  podman image rm registry.gitlab.com/wunker-bunker/blue-build:latest-installer
+  podman image rm ghcr.io/blue-build/cli:latest-installer
 }
 
-podman pull registry.gitlab.com/wunker-bunker/blue-build:latest-installer
+podman pull ghcr.io/blue-build/cli:latest-installer
 
-podman run -d --rm --name blue-build-installer registry.gitlab.com/wunker-bunker/blue-build:latest-installer tail -f /dev/null
+podman run -d --rm --name blue-build-installer ghcr.io/blue-build/cli:latest-installer tail -f /dev/null
 
 set +e
 podman cp blue-build-installer:/out/bb /usr/local/bin/bb
@@ -22,7 +22,7 @@ set -e
 if [ -n $RETVAL ]; then
   cleanup
   echo "Failed to copy file, try:"
-  printf "\tpodman run --rm registry.gitlab.com/wunker-bunker/blue-build:latest-installer | sudo bash\n"
+  printf "\tpodman run --rm ghcr.io/blue-build/cli:latest-installer | sudo bash\n"
   exit 1
 else
   cleanup
