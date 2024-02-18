36f4ac3a4c0fcd1fea6489fe9d7b21c3b91f8b6c
Gerald Pinder <gmpinder@gmail.com>
2024-02-18 11:01:02 -0500

    Just mount the source module instead of using multi-stage

--- src/commands/template.rs
+++ src/commands/template.rs
@@ -1,6 +1,4 @@
 use std::{
-    borrow::Cow,
-    collections::HashSet,
     env, fs,
     path::{Path, PathBuf},
     process,
@@ -223,14 +221,4 @@ fn get_gitlab_registry_path() -> Option<String> {
         )
         .to_lowercase(),
     )
-}
-
-fn get_external_module_images<'a>(modules: &'a [Module]) -> Vec<Cow<'a, str>> {
-    let mut seen = HashSet::new();
-
-    modules
-        .iter()
-        .filter_map(|module| module.source.clone())
-        .filter(|source| seen.insert(source.clone()))
-        .collect::<Vec<_>>()
 }
