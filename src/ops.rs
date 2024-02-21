6cf8052569afdad1d1c23ed0938baea087450536
Gerald Pinder <gmpinder@gmail.com>
2024-02-21 18:21:00 -0500

    Make file append simpler

--- src/ops.rs
+++ src/ops.rs
@@ -1,7 +1,4 @@
-use std::{
-    io::{Read, Seek, SeekFrom, Write},
-    process::Command,
-};
+use std::{io::Write, process::Command};
 
 use anyhow::{anyhow, Result};
 use format_serde_error::SerdeError;
@@ -27,40 +24,16 @@ pub fn check_command_exists(command: &str) -> Result<()> {
     }
 }
 
-pub fn get_last_char(file_path: &str) -> Result<char> {
-    trace!("get_last_char({file_path})");
-    debug!("Getting last character of {file_path}");
-
-    let mut last_char_buffer = vec![0; 4096];
-    let mut file = std::fs::File::open(file_path)?;
-
-    // get the last character
-    file.seek(SeekFrom::Start(file.metadata()?.len() - 1))?;
-    file.read_exact(&mut last_char_buffer[0..1])?;
-
-    Ok(String::from_utf8_lossy(&last_char_buffer)
-        .to_string()
-        .chars()
-        .next()
-        .unwrap_or('\0'))
-}
-
 pub fn append_to_file(file_path: &str, content: &str) -> Result<()> {
     trace!("append_to_file({file_path}, {content})");
     debug!("Appending {content} to {file_path}");
 
-    let last_char = get_last_char(file_path)?;
     let mut file = std::fs::OpenOptions::new()
         .append(true)
         .create(true)
         .open(file_path)?;
 
-    // Check if last character is a newline
-    if last_char != 0xA as char {
-        file.write_all(b"\n")?;
-    }
-
-    file.write_all(format!("{}\n", content).as_bytes())?;
+    writeln!(file, "\n{content}")?;
     Ok(())
 }
 
