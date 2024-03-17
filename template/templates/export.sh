1f2857005bd714e0c10c2d56de806189d41bd5df
Gerald Pinder <gmpinder@gmail.com>
2024-03-17 00:30:26 -0400

    Just finish the actual issue for now

--- template/templates/export.sh
+++ template/templates/export.sh
@@ -1,23 +1,8 @@
+#!/usr/bin/env bash
+
 get_yaml_array() { 
-  set -euo pipefail
   readarray -t "$1" < <(echo "$3" | yq -I=0 "$2")
 } 
 
-rmex() {
-  set -euo pipefail
-  set -x
-  local search_dir="$1"
-  shift
-
-  local exclude_patterns=("$@")
-
-  local grep_pattern=$(printf "|^%s" "${exclude_patterns[@]}")
-  grep_pattern="${grep_pattern:1}"
-
-  find "$search_dir" -type f | grep -Ev "($grep_pattern)" | xargs rm -f
-  set +x
-}
-
 export -f get_yaml_array
-export -f rmex
 export OS_VERSION=$(grep -Po '(?<=VERSION_ID=)\d+' /usr/lib/os-release)
