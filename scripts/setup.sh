d53d609153a7cae8ded1e83f19fb70674afa120a
Gerald Pinder <gmpinder@gmail.com>
2024-05-02 17:09:43 -0400

    Add debian variant

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,13 +7,13 @@ if [ -f /etc/os-release ]; then
     echo "Setting up Alpine based image to run BlueBuild modules"
     apk update
     apk add --no-cache bash curl coreutils wget
-  elif [ "$ID" = "ubuntu" ]; then
+  elif [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
     echo "Setting up Ubuntu based image to run BlueBuild modules"
     apt-get update
-    apt-get install -y bash curl
+    apt-get install -y bash curl coreutils wget
   elif [ "$ID" = "fedora" ]; then
     echo "Settig up Fedora based image to run BlueBuild modules"
-    dnf install -y --refresh bash curl wget
+    dnf install -y --refresh bash curl wget coreutils
   else
     echo "OS not detected, exiting"
     exit 1
