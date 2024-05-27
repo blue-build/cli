9d5b7cccb7136448ae1093bf241ad1916c6dde70
Gerald Pinder <gmpinder@gmail.com>
2024-05-27 12:15:07 -0400

    Revert "Start the migration to using tracing instead of log and env_logger"
    This reverts commit 1be4d89610d13a7477fd432b53d5eb6470f1880b.

--- src/bin/bluebuild.rs
+++ src/bin/bluebuild.rs
@@ -1,38 +1,15 @@
-use anyhow::{anyhow, bail, Result};
 use blue_build::commands::{BlueBuildArgs, BlueBuildCommand, CommandArgs};
-use blue_build_utils::{home_dir, logging};
+use blue_build_utils::logging;
 use clap::Parser;
 use log::LevelFilter;
-use tracing_indicatif::IndicatifLayer;
-use tracing_log::LogTracer;
-use tracing_subscriber::{fmt::FormatEvent, layer::SubscriberExt, util::SubscriberInitExt};
 
-fn main() -> Result<()> {
+fn main() {
     let args = BlueBuildArgs::parse();
 
-    // env_logger::builder()
-    //     .filter_level(args.verbosity.log_level_filter())
-    //     .filter_module("hyper::proto", LevelFilter::Info)
-    //     .format(logging::format_log)
-    //     .init();
-
-    // let logger = LogTracer::new();
-    // log::set_boxed_logger(Box::new(logger))?;
-    // log::set_max_level(args.verbosity.log_level_filter());
-
-    let file_appender = tracing_appender::rolling::hourly(
-        home_dir()
-            .ok_or_else(|| anyhow!("Failed to get home directory"))?
-            .join(".local/share/bluebuild"),
-        "bluebuild.log",
-    );
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    let indicatif_layer = IndicatifLayer::new();
-
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer().with_writer(non_blocking))
-        .with(tracing_subscriber::fmt::layer().with_writer(indicatif_layer.get_stderr_writer()))
-        .with(indicatif_layer)
+    env_logger::builder()
+        .filter_level(args.verbosity.log_level_filter())
+        .filter_module("hyper::proto", LevelFilter::Info)
+        .format(logging::format_log)
         .init();
 
     log::trace!("Parsed arguments: {args:#?}");
@@ -61,6 +38,4 @@ fn main() -> Result<()> {
 
         CommandArgs::Completions(mut command) => command.run(),
     }
-
-    Ok(())
 }
