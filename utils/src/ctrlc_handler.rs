ff3ce4501eb44658e930a078bace1166f2b59bf7
Gerald Pinder <gmpinder@gmail.com>
2024-06-21 23:50:37 -0400

    Switch to signal-hook

--- utils/src/ctrlc_handler.rs
+++ utils/src/ctrlc_handler.rs
@@ -1,14 +1,18 @@
 use std::{
     process,
-    sync::{Arc, Mutex},
+    sync::{atomic::AtomicBool, Arc, Mutex},
+    thread,
 };
 
-use log::error;
+use log::{error, trace, warn};
 use nix::{
-    sys::signal::{kill, Signal::SIGTERM},
+    sys::signal::{kill, Signal},
     unistd::Pid,
 };
 use once_cell::sync::Lazy;
+use signal_hook::{consts::TERM_SIGNALS, flag, iterator::Signals};
+
+use crate::logging::Logger;
 
 static PID_LIST: Lazy<Arc<Mutex<Vec<i32>>>> = Lazy::new(|| Arc::new(Mutex::new(vec![])));
 
@@ -18,18 +22,40 @@ static PID_LIST: Lazy<Arc<Mutex<Vec<i32>>>> = Lazy::new(|| Arc::new(Mutex::new(v
 /// # Panics
 /// Will panic if initialized more than once.
 pub fn init() {
-    let pid_list = PID_LIST.clone();
-    ctrlc::set_handler(move || {
-        let pid_list = pid_list.lock().expect("Should lock mutex");
-        pid_list.iter().for_each(|pid| {
-            if let Err(e) = kill(Pid::from_raw(*pid), SIGTERM) {
-                error!("Failed to kill process {pid}: Error {e}");
+    // Make sure double CTRL+C and similar kills
+    let term_now = Arc::new(AtomicBool::new(false));
+    for sig in TERM_SIGNALS {
+        // When terminated by a second term signal, exit with exit code 1.
+        // This will do nothing the first time (because term_now is false).
+        flag::register_conditional_shutdown(*sig, 1, Arc::clone(&term_now))
+            .expect("Register conditional shutdown");
+        // But this will "arm" the above for the second time, by setting it to true.
+        // The order of registering these is important, if you put this one first, it will
+        // first arm and then terminate ‒ all in the first round.
+        flag::register(*sig, Arc::clone(&term_now)).expect("Register signal");
+    }
+
+    let mut signals = Signals::new(TERM_SIGNALS).expect("Need signal info");
+
+    thread::spawn(move || {
+        signals.forever().for_each(|sig| {
+            if TERM_SIGNALS.contains(&sig) {
+                warn!("Terminating...");
+                Logger::multi_progress().clear().unwrap();
+                let pid_list = PID_LIST.clone();
+                let pid_list = pid_list.lock().expect("Should lock mutex");
+                pid_list.iter().for_each(|pid| {
+                    if let Err(e) = kill(Pid::from_raw(*pid), Signal::SIGTERM) {
+                        error!("Failed to kill process {pid}: Error {e}");
+                    } else {
+                        trace!("Killed process {pid}");
+                    }
+                });
+                drop(pid_list);
+                process::exit(1);
             }
         });
-        drop(pid_list);
-        process::exit(1);
-    })
-    .expect("Should create ctrlc handler");
+    });
 }
 
 /// Add a pid to the list to kill when the program
