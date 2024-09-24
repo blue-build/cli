5cf1ce66a0084d2dea9a3799e47f7ffc5bf152e0
Gerald Pinder <gmpinder@gmail.com>
2024-09-24 12:50:46 -0400

    chore: Check for bootupctl in post-build script

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail
 
-if command -v bootc; then
+if command -v bootupctl && [ -f /usr/lib/ostree-boot/efi/EFI ] && [ "$OS_VERSION" -ge "40" ]; then
+  echo "Generating update metadata"
   bootupctl backend generate-update-metadata
+else
+  echo "Program bootupctl not installed or EFI file not available, skipping..."
 fi
 
 rm -fr /tmp/* /var/*
