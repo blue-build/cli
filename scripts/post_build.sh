f0679fdd11898718f875c0c1db3f5f7d0da519d0
Gerald Pinder <gmpinder@gmail.com>
2024-09-28 20:21:21 -0400

    chore: Remove bootupctl until issue is resolved

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -2,12 +2,13 @@
 
 set -euo pipefail
 
-if command -v bootupctl && [ -f /usr/lib/ostree-boot/efi/EFI ] && [ "$OS_VERSION" -ge "40" ]; then
-  echo "Generating update metadata"
-  bootupctl backend generate-update-metadata
-else
-  echo "Program bootupctl not installed or EFI file not available, skipping..."
-fi
+# See issue https://github.com/coreos/bootupd/issues/635
+# if command -v bootupctl && [ -f /usr/lib/ostree-boot/efi/EFI ] && [ "$OS_VERSION" -ge "40" ]; then
+#   echo "Generating update metadata"
+#   bootupctl backend generate-update-metadata
+# else
+#   echo "Program bootupctl not installed or EFI file not available, skipping..."
+# fi
 
 rm -fr /tmp/* /var/*
 ostree container commit
