0de04175157165072d0ced8ea665bc62c937fc43
Gerald Pinder <gmpinder@gmail.com>
2024-11-22 19:37:54 -0500

    chore: Install jq and prefer over yq for modules

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,14 +6,14 @@ if [ -f /etc/os-release ]; then
   if [ "$ID" = "alpine" ]; then
     echo "Setting up Alpine based image to run BlueBuild modules"
     apk update
-    apk add --no-cache bash curl coreutils wget grep
+    apk add --no-cache bash curl coreutils wget grep jq
   elif [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
     echo "Setting up Ubuntu based image to run BlueBuild modules"
     apt-get update
-    apt-get install -y bash curl coreutils wget
+    apt-get install -y bash curl coreutils wget jq
   elif [ "$ID" = "fedora" ]; then
     echo "Settig up Fedora based image to run BlueBuild modules"
-    dnf install -y --refresh bash curl wget coreutils
+    dnf install -y --refresh bash curl wget coreutils jq
   else
     echo "OS not detected, proceeding without setup"
   fi
