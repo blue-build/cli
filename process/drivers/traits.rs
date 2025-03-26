bd22b77430eabd5f81975938f26ae4a94bc53ed7
Gerald Pinder <gmpinder@gmail.com>
2025-03-26 14:36:02 -0400

    Revert "fix: Fix lints and be sure to login before build in rechunk"
    This reverts commit 51fd86ed6b94b1196b109930170df3cd7d349dcf.

--- process/drivers/traits.rs
+++ process/drivers/traits.rs
@@ -287,8 +287,6 @@ pub trait RechunkDriver: RunDriver + BuildDriver + ContainerMountDriver {
         ))
         .into_diagnostic()?;
 
-        Self::login(true)?;
-
         Self::build(
             &BuildOpts::builder()
                 .image(raw_image.to_string())
