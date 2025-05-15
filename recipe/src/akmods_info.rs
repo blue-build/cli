a81754812a53e2099285b62726bdf469f594019f
Francisco Torres Pérez <franute@gmail.com>
2025-05-15 10:05:29 +0200

    fix: pull akmods-extra only for bazzite (Fixes #441)

--- recipe/src/akmods_info.rs
+++ recipe/src/akmods_info.rs
@@ -3,7 +3,7 @@ use bon::Builder;
 #[derive(Debug, Clone, Builder, PartialEq, Eq, Hash)]
 pub struct AkmodsInfo {
     #[builder(into)]
-    pub images: (String, String, Option<String>),
+    pub images: (String, Option<String>, Option<String>),
 
     #[builder(into)]
     pub stage_name: String,
