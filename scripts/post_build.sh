805cd9629293b2fafbf521da95f4a2ab8d9b573c
bri <284789+b-@users.noreply.github.com>
2025-08-10 03:29:27 -0400

    Update post_build.sh

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -3,14 +3,16 @@
 set -euo pipefail
 . /scripts/exports.sh
 
+shopt -s nullglob
 optdirs=(/opt/*)
 if [[ -n "${optdirs[*]}" ]]; then
+    optfix_dir="/usr/lib/bluebuild-optfix"
+    mkdir -pv "${optfix_dir}"
     echo "Creating symlinks to fix packages that installed to /opt:"
     for optdir in "${optdirs[@]}"; do
         opt=$(basename "${optdir}")
-        lib_opt_dir="/usr/lib/bluebuild-optfix/${opt}"
+        lib_opt_dir="${optfix_dir}/${opt}"
         mv -v "${optdir}" "${lib_opt_dir}"
-        mkdir -pv "${lib_opt_dir}"
         echo "linking ${optdir} => ${lib_opt_dir}"
         echo "L  ${optdir}  -  -  -  -  ${lib_opt_dir}" | tee "/usr/lib/tmpfiles.d/${opt}-bluebuild.conf"
     done
