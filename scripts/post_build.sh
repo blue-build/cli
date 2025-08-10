4ed71f2dae16bc5d007e97c79234b9702090e699
bri <284789+b-@users.noreply.github.com>
2025-08-10 01:42:17 -0400

    add one true optfix
    this (should!) fix anything that ended up in /opt, from e.g., rpm package installs, without needing to manually specify optfix folders in e.g., the dnf or rpm-ostree modules.

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -3,8 +3,24 @@
 set -euo pipefail
 . /scripts/exports.sh
 
+optdirs=(/opt/*)
+if [[ -n "${optdirs[*]}" ]]; then
+
+  echo "Creating symlinks to fix packages that installed to /opt:"
+  for optdir in "${optdirs[@]}"; do
+    opt=$(basename "${optdir}")
+    lib_opt_dir="/usr/lib/opt/${opt}"
+    mv "${optdir}" "${lib_opt_dir}"
+    mkdir -p "${lib_opt_dir}"
+    tee "/usr/lib/tmpfiles.d/${opt}-bluebuild.conf" <<EOF
+# create a link from ${optdir} to ${lib_opt_dir}
+L  ${optdir}  -  -  -  -  ${lib_opt_dir}
+EOF
+  done
+fi
+
 rm -rf /tmp/* /var/*
 
-if feature_enabled "bootc" && command -v bootc > /dev/null; then
+if feature_enabled "bootc" && command -v bootc >/dev/null; then
   bootc container lint
 fi
