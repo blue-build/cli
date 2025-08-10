08eb04885c523ce23e0ee3b25a9af120e414d087
bri <284789+b-@users.noreply.github.com>
2025-08-10 02:59:30 -0400

    add one true optfix
    this (should!) fix anything that ended up in /opt, from e.g., rpm package installs, without needing to manually specify optfix folders in e.g., the dnf or rpm-ostree modules.

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -5,18 +5,15 @@ set -euo pipefail
 
 optdirs=(/opt/*)
 if [[ -n "${optdirs[*]}" ]]; then
-
-  echo "Creating symlinks to fix packages that installed to /opt:"
-  for optdir in "${optdirs[@]}"; do
-    opt=$(basename "${optdir}")
-    lib_opt_dir="/usr/lib/opt/${opt}"
-    mv "${optdir}" "${lib_opt_dir}"
-    mkdir -p "${lib_opt_dir}"
-    tee "/usr/lib/tmpfiles.d/${opt}-bluebuild.conf" <<EOF
-# create a link from ${optdir} to ${lib_opt_dir}
-L  ${optdir}  -  -  -  -  ${lib_opt_dir}
-EOF
-  done
+    echo "Creating symlinks to fix packages that installed to /opt:"
+    for optdir in "${optdirs[@]}"; do
+        opt=$(basename "${optdir}")
+        lib_opt_dir="/usr/lib/bluebuild-optfix/${opt}"
+        mv -v "${optdir}" "${lib_opt_dir}"
+        mkdir -pv "${lib_opt_dir}"
+        echo "linking ${optdir} => ${lib_opt_dir}"
+        echo "L  ${optdir}  -  -  -  -  ${lib_opt_dir}" | tee "/usr/lib/tmpfiles.d/${opt}-bluebuild.conf"
+    done
 fi
 
 rm -rf /tmp/* /var/*
