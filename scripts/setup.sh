9278d957930cd971487e0e4120c554c36e5aea35
Gerald Pinder <gmpinder@gmail.com>
2025-12-22 12:59:15 -0500

    feat(bootc): Add support for new package managers
    This goes hand in hand with the new modules created in https://github.com/blue-build/modules/pull/523

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
 
-if [ -f /etc/os-release ]; then
-  # Initialize variable to store the ID
-  ID=""
-
-  # Read the /etc/os-release file line by line
-  while IFS== read -r key value; do
-    # Check if the key is 'ID'
-    if [ "$key" = "ID" ]; then
-      # Remove any quotes from the value and store it in id variable
-      ID=$(echo "$value" | tr -d '"')
-      break
-    fi
-  done < /etc/os-release
-
-  if [ "$ID" = "alpine" ]; then
-    echo "Setting up Alpine based image to run BlueBuild modules"
-    apk update
-    apk add --no-cache bash curl coreutils wget grep jq
-  elif [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
-    echo "Setting up Ubuntu based image to run BlueBuild modules"
-    export DEBIAN_FRONTEND=noninteractive
+# TODO: Remove once we're all POSIX https://github.com/blue-build/modules/issues/503
+if [ -x /usr/bin/dnf5 ] \
+    || [ -x /bin/dnf5 ] \
+    || [ -x /sbin/dnf5 ]; then
+    dnf5 -y install bash curl wget coreutils jq grep
+elif [ -x /usr/bin/dnf4 ] \
+    || [ -x /bin/dnf4 ] \
+    || [ -x /sbin/dnf4 ]; then
+    dnf4 -y install bash curl wget coreutils jq grep
+elif [ -x /usr/bin/zypper ] \
+    || [ -x /bin/zypper ] \
+    || [ -x /sbin/zypper ]; then
+    zypper --non-interactive install --auto-agree-with-licenses bash curl wget coreutils jq grep find
+elif [ -x /usr/bin/pacman ] \
+    || [ -x /bin/pacman ] \
+    || [ -x /sbin/pacman ]; then
+    pacman --sync --noconfirm --refresh --sysupgrade bash curl wget coreutils jq grep
+elif [ -x /usr/bin/apt-get ] \
+    || [ -x /bin/apt-get ] \
+    || [ -x /sbin/apt-get ]; then
     apt-get update
-    apt-get install -y bash curl coreutils wget jq
-  elif [ "$ID" = "fedora" ]; then
-    echo "Settig up Fedora based image to run BlueBuild modules"
-    dnf install -y --refresh bash curl wget coreutils jq
-  else
-    echo "OS not detected, proceeding without setup"
-  fi
+    DEBIAN_FRONTEND=noninteractive apt-get -y install bash curl wget coreutils jq grep
+elif [ -x /usr/bin/apk ] \
+    || [ -x /bin/apk ] \
+    || [ -x /sbin/apk ]; then
+    apk add --no-cache bash curl coreutils wget grep jq
 fi
