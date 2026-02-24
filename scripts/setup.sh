d4cc81f57c7a6d48ab7e247ad5bb33678cf5e965
Gerald Pinder <gmpinder@gmail.com>
2026-02-23 19:41:19 -0500

    fix: Only run setup if required programs don't exist (#712)
    This PR removes `wget` from the list of programs to install from setup
    since we don't use it anymore in our modules. This also adds a check for
    all the programs we need before running any of the package managers.
    This was requested by the secureblue folks to help harden their builds.
    Since `dnf` apparently reaches out to non-secure HTTP sites, it would
    cause their network hardener to flag it and since this script is run
    before any modules are called it couldn't be manually stopped.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 
 # TODO: Remove once we're all POSIX https://github.com/blue-build/modules/issues/503
-if [ -x /usr/bin/dnf5 ] \
-    || [ -x /bin/dnf5 ] \
-    || [ -x /sbin/dnf5 ]; then
-    dnf5 -y install bash curl wget coreutils jq grep
-elif [ -x /usr/bin/dnf4 ] \
-    || [ -x /bin/dnf4 ] \
-    || [ -x /sbin/dnf4 ]; then
-    dnf4 -y install bash curl wget coreutils jq grep
-elif [ -x /usr/bin/zypper ] \
-    || [ -x /bin/zypper ] \
-    || [ -x /sbin/zypper ]; then
-    zypper --non-interactive install --auto-agree-with-licenses bash curl wget coreutils jq grep find
-elif [ -x /usr/bin/pacman ] \
-    || [ -x /bin/pacman ] \
-    || [ -x /sbin/pacman ]; then
-    pacman --sync --noconfirm --refresh --sysupgrade bash curl wget coreutils jq grep
-elif [ -x /usr/bin/apt-get ] \
-    || [ -x /bin/apt-get ] \
-    || [ -x /sbin/apt-get ]; then
-    apt-get update
-    DEBIAN_FRONTEND=noninteractive apt-get -y install bash curl wget coreutils jq grep
-elif [ -x /usr/bin/apk ] \
-    || [ -x /bin/apk ] \
-    || [ -x /sbin/apk ]; then
-    apk add --no-cache bash curl coreutils wget grep jq
+command_exists() {
+    [ -x "/usr/bin/$1" ] || [ -x "/bin/$1" ] || [ -x "/usr/sbin/$1" ] || [ -x "/sbin/$1" ]
+}
+
+if ! command_exists bash \
+    || ! command_exists jq \
+    || ! command_exists curl \
+    || ! command_exists grep \
+    || ! command_exists ls; then
+    if command_exists dnf5; then
+        dnf5 -y install bash curl coreutils jq grep
+    elif command_exists dnf4; then
+        dnf4 -y install bash curl coreutils jq grep
+    elif command_exists zypper; then
+        zypper --non-interactive install --auto-agree-with-licenses bash curl coreutils jq grep find
+    elif command_exists pacman; then
+        pacman --sync --noconfirm --refresh --sysupgrade bash curl coreutils jq grep
+    elif command_exists apt-get; then
+        apt-get update
+        DEBIAN_FRONTEND=noninteractive apt-get -y install bash curl coreutils jq grep
+    elif command_exists apk; then
+        apk add --no-cache bash curl coreutils grep jq
+    fi
 fi
